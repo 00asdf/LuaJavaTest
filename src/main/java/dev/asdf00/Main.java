8bedcbdd546665b6137a73a944f2d011e45a5f58
00asdf <00asdf@users.noreply.github.com>
2024-03-13 23:45:07 +0100

    initial commit

---
+++ src/main/java/dev/asdf00/Main.java
@@ -0,0 +1,10 @@
+package dev.asdf00;
+
+import party.iroiro.luajava.lua54.Lua54;
+
+public class Main {
+    public static void main(String[] args) {
+        var lua = new Lua54();
+        System.out.println("Hello world!");
+    }
+}
